03e5fb82e304da2f6201001c1440b9efbe813f22
Rovijun <rovijun@gmail.com>
2021-11-30 12:24:54 +0100

    fixed ex00

--- Day01/Ex00/program.js
+++ Day01/Ex00/program.js
@@ -3,7 +3,7 @@ function tabTriCarre(array) {
     for (let i = 0; i < array.length; i++) {
       result[i] = array[i] ** 2;
     }
-    return result
+    return result.sort(function(a, b) {return a - b;})
   }  
 
 // Merci de ne pas effacer la ligne en dessous.
